24d0a5e0502094e427efc92c21a5b76d4ab9f201
chlrbtjs <gyusun.choi@gmail.com>
2024-07-09 22:10:05 +0900

    주석

--- modules/card.ts
+++ modules/card.ts
@@ -1,15 +1,15 @@
 import state from "./state"
 
 interface Icard {
-  cardcode: number,
-  cardname: string,
-  classes: 1 | 2 | 3 | 4,
-  canMissionary: boolean,
-  keyWords: string[],
-  createInfluence: number,
+  cardcode: number,           // 카드 코드, 코드 부여 규칙은 미정
+  cardname: string,           // 카드 이름
+  classes: 1 | 2 | 3 | 4,     // 계급
+  canMissionary: boolean,     // 포교 할 수 있는지
+  keyWords: string[],         // 도발등의 키워드
+  createInfluence: number,    // 세력 생성치
 
-  whenOpen: (state: state) => state,
-  whenInfluentialization: (state: state) => state,
+  whenOpen: (state: state) => state,                // 오픈 시 효과
+  whenInfluentialization: (state: state) => state,  // 세력화 시 효과
 }
 
 abstract class card implements Icard {
@@ -26,6 +26,10 @@ abstract class card implements Icard {
     this.classes = classes;
     this.canMissionary = canMissionary;
     this.keyWords = keyWords;
+    this.createInfluence = this.classes;
+    if (this.classes === 4) {
+      this.createInfluence = 5;
+    }
   }
   
   whenOpen = (state: state): state => {
